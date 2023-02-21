3a92cd8b18ec724dabea3cc92c0327d619b7ca86
ljh415 <ljh415@gmail.com>
2023-02-21 18:32:05 +0900

    fix some bugs, modelsignature, conda_env,

--- train.py
+++ train.py
@@ -1,45 +1,20 @@
-import os
-import time
 import argparse
-import numpy as np
-from tqdm import tqdm
-from collections import defaultdict
 
 import torch
-import torch.nn as nn
-import torchvision.datasets as datasets
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader, random_split
-
 from torchmetrics import Accuracy
 
 import mlflow
 from mlflow.models.signature import ModelSignature
 from mlflow.types.schema import Schema, TensorSpec
 
-from model import TestResNet
+from model import TestResNet, PlainCNN
+from dataset import Cifar10DataLoader
 from utils import fix_seed, get_now
 
 fix_seed()
 
 def train():
-    conda_env = {
-        'channels': ['conda-forge'],
-        'dependencies': [
-            'python=3.9.16',
-            'pip<=22.3.1'],
-        'pip': [
-            'mlflow<3,>=2.1',
-            'cloudpickle==2.2.1',
-            'ipython==8.10.0',
-            'torchmetrics==0.11.1',
-            'torch==1.12.1',
-            'torchvision==0.13.1',
-            '--extra-index-url https://download.pytorch.org/whl/cu113'
-        ],
-        'name': 'mlflow-env'
-    }
-    
     device = f"cuda:{args.device_id}" if args.cuda else "cpu"
     
     transform = transforms.Compose([
@@ -50,140 +25,155 @@ def train():
     ])
     
     # dataset
-    data = datasets.CIFAR10(root='~/data', train=True, transform=transform, download=True)
-    train_data, valid_data = random_split(data, [int(len(data)*0.8), len(data)-int(len(data)*0.8)])
+    train_dataloader, valid_dataloader = Cifar10DataLoader(train=True, transform=transform).get_dataloader(args.batch_size, args.num_workers)
     
-    train_dataloader = DataLoader(train_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
-    valid_dataloader = DataLoader(valid_data, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers, pin_memory=True)
-    
+    # metric
     metric_acc = Accuracy(task='multiclass', num_classes=10).to(device)
     
     # model
-    model = TestResNet(init_layers=args.init_layers).to(device)
+    if args.model == 'plain_cnn':
+        model = PlainCNN().to(device)
+    elif args.model == 'resnet':
+        model = TestResNet(init_layers=args.init_layers).to(device)
+    else:
+        raise Exception("Unsupported model")
     
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
     criterion = torch.nn.CrossEntropyLoss().to(device)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=4, mode='min', factor=0.5, verbose=True)
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=50, T_mult=2, eta_min=0.001)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, mode='min', factor=0.5, verbose=True)
     
-    
-    with mlflow.start_run(
-        run_name = f"test_{get_now(True)}"
-        ) as run:
-        
-
-        # logging_params
+    if args.mlflow:
+        run_name = args.run_name if args.run_name is not None else f"test_{get_now(True)}"
+        mlflow.start_run(run_name=run_name)
         mlflow.log_params({
             'init_lr': args.lr,
+            'model': args.model
         })
+    
+    for epoch in range(args.epochs):
+        current_lr = scheduler.optimizer.param_groups[0]['lr']
         
-        for epoch in range(args.epochs):
-            cur_lr = scheduler.optimizer.param_groups[0]['lr']
+        model.train()
+        
+        step = 0
+        running_loss = 0
+        running_acc = 0
+        
+        for batch_idx, (img_batch, target_batch) in enumerate(train_dataloader):
             
-            model.train()
+            optimizer.zero_grad()
             
-            step = 0
-            running_loss = 0
-            running_acc = 0
+            img = img_batch.to(device)
+            target = target_batch.to(device)
             
+            pred = model(img)
+
+            loss = criterion(pred, target)
+            acc = metric_acc(pred, target)
             
-            for batch_idx, (img_batch, target_batch) in enumerate(train_dataloader):
-                # now_lr = args.lr
-                
-                if batch_idx == 50:
-                    break
-                
-                optimizer.zero_grad()
+            running_loss += loss.detach().cpu()
+            running_acc += acc.detach().cpu()
+            
+            loss.backward()
+            optimizer.step()
+            
+            step += 1
+            
+            status = (
+                "\r> epoch: {:3d} > step: {:3d} > loss: {:.3f}, lr: {:.3f}, acc: {:.2f}".format(
+                    epoch+1, step, running_loss/(batch_idx+1), current_lr, running_acc/(batch_idx+1)
+                )
+            )
+            
+            print(status, end="")
+        train_loss = running_loss / len(train_dataloader)
+        train_acc = running_acc / len(train_dataloader)
+        print()
+        
+        ## validate
+        running_val_loss = 0
+        running_val_acc = 0
+        
+        with torch.no_grad():
+            model.eval()
+            for batch_idx, (img_batch, target_batch) in enumerate(valid_dataloader):
                 
                 img = img_batch.to(device)
                 target = target_batch.to(device)
                 
                 pred = model(img)
-
-                loss = criterion(pred, target)
-                acc = metric_acc(pred, target)
                 
-                running_loss += loss.detach().cpu()
-                running_acc += acc.detach().cpu()
+                val_loss = criterion(pred, target)
+                val_acc = metric_acc(pred, target)
                 
-                loss.backward()
-                optimizer.step()
-                
-                step += 1
+                running_val_loss += val_loss.detach().cpu()
+                running_val_acc += val_acc.detach().cpu()
                 
                 status = (
-                    "\r> epoch: {:3d} > step: {:3d} > loss: {:.3f}, lr: {:.3f}, acc: {:.2f}".format(
-                        epoch+1, step, running_loss/(batch_idx+1), cur_lr, running_acc/(batch_idx+1)
+                    "\r validation : {:6d} / {:6d}".format(
+                        batch_idx+1,
+                        len(valid_dataloader)
                     )
                 )
-                
                 print(status, end="")
-            train_loss = running_loss / len(train_dataloader)
-            train_acc = running_acc / len(train_dataloader)
-            
-            print()
-            ## validate
-            
-            running_val_loss = 0
-            running_val_acc = 0
-            
-            with torch.no_grad():
-                model.eval()
-                for batch_idx, (img_batch, target_batch) in enumerate(valid_dataloader):
-                    
-                    if batch_idx == 50:
-                        break
-                    
-                    img = img_batch.to(device)
-                    target = target_batch.to(device)
-                    
-                    pred = model(img)
-                    
-                    val_loss = criterion(pred, target)
-                    val_acc = metric_acc(pred, target)
-                    
-                    running_val_loss += val_loss.detach().cpu()
-                    running_val_acc += val_acc.detach().cpu()
-                    
-                    status = (
-                        "\r validation : {:6d} / {:6d}".format(
-                            batch_idx+1,
-                            len(valid_dataloader)
-                        )
-                    )
-                    print(status, end="")
-            
-            val_loss = running_val_loss / len(valid_dataloader)
-            val_acc = running_val_acc / len(valid_dataloader)
-            print("\nValidation loss: {:3f}, acc: {:.2f}\n".format(val_loss, val_acc))
-            
-            scheduler.step(metrics=val_loss)
-            
-            ### mlflow logging
+        
+        val_loss = running_val_loss / len(valid_dataloader)
+        val_acc = running_val_acc / len(valid_dataloader)
+        print("\nValidation loss: {:3f}, acc: {:.2f}\n".format(val_loss, val_acc))
+        
+        scheduler.step(metrics=val_loss)
+        
+        if args.mlflow:
             mlflow.log_metrics({
                 'train_accuracy': train_acc.numpy().item(),
                 'train_loss': train_loss.numpy().item(),
                 'validation_accuracy': val_acc.numpy().item(),
                 'validation_loss': val_loss.numpy().item(),
-                'lr': cur_lr
+                'lr': current_lr
             })
+    
+    if args.mlflow:
+        # input_schema = Schema([
+        #     TensorSpec(np.dtype(np.float32), (-1, 3, 32, 32))
+        # ])
+        # output_schema = Schema([
+        #     TensorSpec(np.dtype(np.float32), (-1, 10))
+        # ])
         
-        # model_signature = infer_signature(train_data, pred)
-        input_schema = Schema([
-            TensorSpec(np.dtype(np.float32), (-1, 3, 32, 32))
-        ])
-        output_schema = Schema([
-            TensorSpec(np.dtype(np.float32), (-1, 10))
-        ])
+        conda_env = {
+            'name': 'mlflow-env',
+            'channels': ['conda-forge'],
+            'dependencies': [
+                'python=3.9.16',
+                'pip<=22.3.1',
+                {'pip': [
+                    'mlflow<3,>=2.1',
+                    'cloudpickle==2.2.1',
+                    'ipython==8.10.0',
+                    'torch==1.12.1',
+                    'torchvision==0.13.1',
+                    '--extra-index-url https://download.pytorch.org/whl/cu113',
+                    'torchmetrics==0.11.1',
+                ]}
+            ],
+        }
+        
         mlflow.pytorch.log_model(
             model,
             artifact_path="test_resnet",
-            signature=ModelSignature(inputs=input_schema, outputs=output_schema),
+            # signature=ModelSignature(inputs=input_schema, outputs=output_schema),
             conda_env=conda_env
         )
         print("Model saved in run %s" % mlflow.active_run().info.run_uuid)
+        mlflow.end_run()
+
+def test():
     
+    pass
+
+def main():
+    pass
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -194,8 +184,11 @@ if __name__ == "__main__":
     parser.add_argument("--cuda", type=bool, default=True)
     parser.add_argument("--num_workers", type=int, default=4)
     parser.add_argument("--init_layers", type=int, default=1)
+    parser.add_argument("--mlflow", action="store_true")
+    parser.add_argument("--model", type=str, default='plain_cnn')
+    parser.add_argument("--run_name", type=str, default=None)
     
-    args = parser.parse_args()    
+    args = parser.parse_args()
     
     train()
     
